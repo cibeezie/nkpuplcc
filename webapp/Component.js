b9a602c03b67a6197c079c28174ed085223ea9df
Stanislaw Kubiak <stanislaw.kubiak@promos-consult.de>
2020-05-18 15:38:11 +0000

    Initial commit

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("PROHAN.prohan_re_nkpu_pl_cc.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
